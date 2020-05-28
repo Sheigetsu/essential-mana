b295033d6823f9b6175ef14e250abe52205e2129
Sheigetsu <sheigetsu@mailbox.org>
2020-05-28 16:51:27 +0200

    bootstrap Pog

--- src/components/layouts/postsLayout.tsx
+++ src/components/layouts/postsLayout.tsx
@@ -1,24 +1,44 @@
 import React from "react";
-import {MDXProvider} from "@mdx-js/react";
+import { MDXProvider } from "@mdx-js/react";
 import ItemTooltip from "../itemTooltip";
 import Header from "../header";
 import Footer from "../footer";
+import { graphql, useStaticQuery } from "gatsby";
+import Badge from "react-bootstrap/Badge";
 
 const shortcodes = {
     ItemTooltip
-}
+};
 
-const PostsLayout = ({children: content}) => {
+const PostsLayout = ({ children: content }) => {
+    const data = useStaticQuery(graphql`
+      query {
+        allMdx {
+          nodes {
+            frontmatter {
+              title
+              author
+              description
+              date(formatString: "MMMM DD, YYYY")
+            }
+          }
+        }
+      }
+    `);
+
     return (
         <MDXProvider components={shortcodes}>
-            <Header />
+            <Header/>
             <div>
-                <h1>Testing</h1>
+                <div>
+                    <h1>{data.allMdx.nodes[0].frontmatter.title}<Badge variant="secondary">{data.allMdx.nodes[0].frontmatter.author}</Badge></h1>
+                    <sub>{data.allMdx.nodes[0].frontmatter.date}</sub>
+                </div>
                 <div>{content}</div>
             </div>
-            <Footer />
+            <Footer/>
         </MDXProvider>
     );
-}
+};
 
 export default PostsLayout;

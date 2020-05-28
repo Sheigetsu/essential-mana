92cfdc1ec0606fb77873576f436b638191e8c0f2
LLoyderino <adrijan.bjedov@gmail.com>
2020-05-28 16:52:55 +0200

    moved header/footer data logic to their component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,22 +1,36 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { useStaticQuery, graphql } from "gatsby";
 
-const Header = ({ siteTitle, categories }) => (
-  <header>
-    <div>
-      <ul>
-        <li>
-          <a href="/">{siteTitle}</a>
-        </li>
-        {categories.map(link => {
+const Header = () => {
+  // add categories / tags query
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+
+  return (
+    <header>
+      <div>
+        <ul>
           <li>
-            <a href={link.url}>{link.text}</a>
-          </li>;
-        })}
-      </ul>
-    </div>
-  </header>
-);
+            <a href="/">{data.site.siteMetadata.title}</a>
+          </li>
+          {/* {categories.map(link => {
+            <li>
+              <a href={link.url}>{link.text}</a>
+            </li>;
+          })} */}
+        </ul>
+      </div>
+    </header>
+  );
+};
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
--- src/components/layouts/defaultLayout.tsx
+++ src/components/layouts/defaultLayout.tsx
@@ -2,46 +2,23 @@
 import React from "react";
 // @ts-ignore
 import PropTypes from "prop-types";
-import {useStaticQuery, graphql} from "gatsby";
 
 import Header from "../header";
 import Footer from "../footer";
 
-const DefaultLayout = ({children: content}) => {
-    const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-          author
-        }
-      }
-    }
-  `);
-
-    const temp = [
-        {
-            url: "/awoo",
-            text: "achoo",
-        },
-        {
-            url: "/awoo2",
-            text: "achoo2",
-        },
-    ];
-
-    return (
-        <>
-            <Header siteTitle={data.site.siteMetadata.title} categories={temp}/>
-            <main>{content}</main>
-            <Footer author={data.site.siteMetadata.author}/>
-        </>
-    );
+const DefaultLayout = ({ children: content }) => {
+  return (
+    <>
+      <Header />
+      <main>{content}</main>
+      <Footer />
+    </>
+  );
 };
 
 DefaultLayout.propTypes = {
-    children: PropTypes.node.isRequired,
-    categories: PropTypes.array,
+  children: PropTypes.node.isRequired,
+  categories: PropTypes.array,
 };
 
 export default DefaultLayout;

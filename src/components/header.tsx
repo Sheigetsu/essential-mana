fac892092cd7ad049ba49dddca28d6d746bade74
Sheigetsu <sheigetsu@mailbox.org>
2020-05-29 14:53:14 +0200

    i hate git

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,32 +1,74 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Button from "react-bootstrap/Button";
+import { useStaticQuery, graphql } from "gatsby";
 
-const Header = ({ siteTitle, categories }) => (
+const Header = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMdx {
+        edges {
+          node {
+            frontmatter {
+              tags
+              title
+            }
+          }
+        }
+      }
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+
+  const removeDupes = (data: string[]): string[] => {
+    let result = [];
+
+    for (let tag of data) {
+      if (!result.includes(tag)) result.push(tag);
+    }
+
+    return result;
+  };
+
+  const title = data.site.siteMetadata.title;
+  const tags = removeDupes(
+    [].concat.apply(
+      [],
+      data.allMdx.edges.map(edge => edge.node.frontmatter.tags)
+    )
+  );
+
+  return (
     <header>
-        <div>
-            <ul>
-                <li>
-                    <a href="/">{siteTitle}</a>
-                </li>
-                {categories.map(link => {
-                    <li>
-                        <a href={link.url}>{link.text}</a>
-                    </li>;
-                })}
-            </ul>
-        </div>
+      <div>
+        <ul>
+          <li>
+            <a href="/">{title}</a>
+          </li>
+          {tags.map(tag => {
+            return (
+              <li>
+                <a href="#">{tag}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </header>
-);
+  );
+};
 
 Header.propTypes = {
-    siteTitle: PropTypes.string,
-    categories: PropTypes.array
+  siteTitle: PropTypes.string,
+  categories: PropTypes.array,
 };
 
 Header.defaultProps = {
-    siteTitle: `Essential-mana`,
-    categories: []
+  siteTitle: ``,
+  categories: [],
 };
 
 export default Header;

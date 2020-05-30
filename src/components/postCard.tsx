f51b96547aa824d31311d299db05b7c4f5045671
Sheigetsu <marvin.meyer@mailbox.org>
2020-05-30 21:52:41 +0200

    how to cards?

--- src/components/postCard.tsx
+++ src/components/postCard.tsx
@@ -1,17 +1,32 @@
 import React from "react";
 import { Link } from "gatsby";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
 const PostCard = ({ title, tags }) => (
-  <div>
-    {title}
-    {tags.map(tag => {
-      return (
-        <Link key={tag} to="/" activeStyle={{ paddingLeft: "10px" }}>
-          #{tag}
-        </Link>
-      );
-    })}
-  </div>
+  // <div className={"post-card"}>
+  //   <div className={"post-card-header"}>
+  //     <img src={"https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"}  alt={"baby-yoda"}/>
+  //     <div className={"title"}>{title}</div>
+  //   </div>
+  //   {tags.map(tag => {
+  //     return (
+  //       <Link key={tag} to="/" activeStyle={{ paddingLeft: "10px" }}>
+  //         #{tag}
+  //       </Link>
+  //     );
+  //   })}
+  // </div>
+    <Card>
+        <Card.Img variant="top" src="https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"/>
+        <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+            </Card.Text>
+        </Card.Body>
+    </Card>
 );
 
 export default PostCard;

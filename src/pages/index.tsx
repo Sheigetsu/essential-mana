578ba5c9f6b5ec13d9615d8183ab55cddef6880e
LLoyderino <adrijan.bjedov@gmail.com>
2020-06-02 14:49:54 +0200

    "featured post" for odd amounts of posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,58 +8,86 @@ import PostCard from "../components/postCard";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
+const FeaturedPostCard = ({ card }) => {
+  if (!card) return null;
+
+  const postCard = card[0][0];
+  return (
+    <Row>
+      <Col key={postCard.id} md={12} xs={12}>
+        <PostCard
+          key={postCard.id}
+          title={postCard.frontmatter.title}
+          tags={postCard.frontmatter.tags}
+          author={postCard.frontmatter.author}
+          slug={postCard.frontmatter.slug}
+          description={postCard.frontmatter.description}
+          date={postCard.frontmatter.date}
+          thumbnail={postCard.frontmatter.thumbnail}
+        />
+      </Col>
+    </Row>
+  );
+};
+
 const IndexPage = () => {
-    const data = useStaticQuery(graphql`
-        query {
-          allMdx(sort: {fields: frontmatter___date, order: ASC}) {
-            edges {
-              node {
-                frontmatter {
-                  tags
-                  title
-                  author
-                  slug
-                  date(formatString: "dddd MMMM Do, YYYY")
-                  description
-                  thumbnail
-                }
-                id
-              }
+  const data = useStaticQuery(graphql`
+    query {
+      allMdx(sort: { fields: frontmatter___date, order: ASC }) {
+        edges {
+          node {
+            frontmatter {
+              tags
+              title
+              author
+              slug
+              date(formatString: "dddd MMMM Do, YYYY")
+              description
+              thumbnail
             }
+            id
           }
         }
-    `);
-    const postCardInfo = data.allMdx.edges.map(edge => edge.node);
-    const chunkedPostCards = _.chunk(postCardInfo, 2);
-    const sortedPostCards = chunkedPostCards.reverse();
+      }
+    }
+  `);
+  const postCardInfo = data.allMdx.edges.map(edge => edge.node);
+  const chunkedPostCards = _.chunk(postCardInfo, 2);
+  const sortedPostCards = chunkedPostCards.reverse();
+  const featured = postCardInfo.length % 2 != 0;
 
-    return (
-        <DefaultLayout>
-            <div className={"main-page"}>
-                {sortedPostCards.map((card, index) => {
-                    return (
-                        // using index as key isn't really recommended but might be fine
-                        <Row key={index}>
-                            {card.map(postCard => {
-                                return (
-                                    <Col key={postCard.id}>
-                                        <PostCard key={postCard.id}
-                                                  title={postCard.frontmatter.title}
-                                                  tags={postCard.frontmatter.tags}
-                                                  author={postCard.frontmatter.author}
-                                                  slug={postCard.frontmatter.slug}
-                                                  description={postCard.frontmatter.description}
-                                                  date={postCard.frontmatter.date}
-                                                  thumbnail={postCard.frontmatter.thumbnail}/>
-                                    </Col>
-                                );
-                            })}
-                        </Row>
-                    );
-                })}
-            </div>
-        </DefaultLayout>
-    );
+  return (
+    <DefaultLayout>
+      <div className={"main-page"}>
+        <FeaturedPostCard
+          card={featured ? sortedPostCards.splice(0, 1) : undefined}
+        />
+        {sortedPostCards.map((card, index) => {
+          return (
+            // using index as key isn't really recommended but might be fine
+            <Row key={index}>
+              {card.map(postCard => {
+                return (
+                  <Col key={postCard.id} md={6} xs={12}>
+                    <PostCard
+                      key={postCard.id}
+                      title={postCard.frontmatter.title}
+                      tags={postCard.frontmatter.tags}
+                      author={postCard.frontmatter.author}
+                      slug={postCard.frontmatter.slug}
+                      description={postCard.frontmatter.description}
+                      date={postCard.frontmatter.date}
+                      thumbnail={postCard.frontmatter.thumbnail}
+                    />
+                  </Col>
+                );
+              })}
+            </Row>
+          );
+        })}
+      </div>
+    </DefaultLayout>
+  );
 };
 
 export default IndexPage;

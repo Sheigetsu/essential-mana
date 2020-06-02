894ce451dda447cb43a8cfd61e006d261ae7054c
LLoyderino <adrijan.bjedov@gmail.com>
2020-06-02 16:36:07 +0200

    readaptation to new card style

--- src/pages/searchTesting.tsx
+++ src/pages/searchTesting.tsx
@@ -1,21 +1,28 @@
 // @ts-ignore
 import React, { useState } from "react";
 import { useStaticQuery, graphql } from "gatsby";
+import _ from "lodash";
 
 import DefaultLayout from "../components/layouts/defaultLayout";
 import PostCard from "../components/postCard";
 import Search from "../components/search";
+import { Row, Col } from "react-bootstrap";
 
 const SearchTesting = () => {
   const [search, setSearch] = useState("");
   const data = useStaticQuery(graphql`
     query {
-      allMdx {
+      allMdx(sort: { fields: frontmatter___date, order: ASC }) {
         edges {
           node {
             frontmatter {
               tags
               title
+              author
+              slug
+              date(formatString: "dddd MMMM Do, YYYY")
+              description
+              thumbnail
             }
             id
           }
@@ -26,14 +33,17 @@ const SearchTesting = () => {
 
   const postCardInfo = data.allMdx.edges.map(edge => edge.node);
 
-  const filtered = postCardInfo.filter(post => {
-    if (post.frontmatter.title.toUpperCase().includes(search.toUpperCase()))
-      return true;
-    if (!post.frontmatter.tags) return false;
-    for (let tag of post.frontmatter.tags)
-      if (tag.toUpperCase().includes(search.toUpperCase())) return true;
-    return false;
-  });
+  const filtered = _.chunk(
+    postCardInfo.filter(post => {
+      if (post.frontmatter.title.toUpperCase().includes(search.toUpperCase()))
+        return true;
+      if (!post.frontmatter.tags) return false;
+      for (let tag of post.frontmatter.tags)
+        if (tag.toUpperCase().includes(search.toUpperCase())) return true;
+      return false;
+    }),
+    3
+  );
 
   const onSearch = event => {
     setSearch(event.target.value);
@@ -43,13 +53,26 @@ const SearchTesting = () => {
     <DefaultLayout>
       <Search onFilterChange={e => onSearch(e)} />
       <div style={{ paddingTop: "10px" }}>
-        {filtered.map(post => {
+        {filtered.map((owo, index) => {
           return (
-            <PostCard
-              key={post.id}
-              title={post.frontmatter.title}
-              tags={post.frontmatter.tags || []}
-            />
+            <Row key={index}>
+              {owo.map((postCard: any) => {
+                return (
+                  <Col key={postCard.id} md={4}>
+                    <PostCard
+                      key={postCard.id}
+                      title={postCard.frontmatter.title}
+                      tags={postCard.frontmatter.tags}
+                      author={postCard.frontmatter.author}
+                      slug={postCard.frontmatter.slug}
+                      description={postCard.frontmatter.description}
+                      date={postCard.frontmatter.date}
+                      thumbnail={postCard.frontmatter.thumbnail}
+                    />
+                  </Col>
+                );
+              })}
+            </Row>
           );
         })}
       </div>
